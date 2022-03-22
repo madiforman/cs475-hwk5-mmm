04fc1759dea88c9b390ead143ed1f4c2a994b785
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-03-21 22:18:39 -0400

    i feel defeated

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -6,18 +7,76 @@
 #include "mmm.h"
 
 /* globals (anything here would be shared with all threads) */
+double **mtrx_a;
+double **mtrx_b;
+double **mtrx_c;
+int SIZE;
+int t_num;
 
 int main(int argc, char *argv[])
 {
-	double clockstart, clockend;
-	clockstart = rtclock(); // start clocking
 
-	// start: stuff I want to clock
+	if (argc == 1 || argc == 2)
+	{
+		printf("Usage: ./mmm <mode> [num threads] <size>\n");
+	}
+	else if ((strcmp(argv[1], "P") == 0) && argc == 3)
+	{
+		printf("Error: parallel mode requires <size>\n");
+	}
+	// else if (strcmp(argv[1], "S"))
+	// {
+	// 	SIZE = atoi(argv[2]);
+	// 	mtrx_a = mmm_init(SIZE);
+	// 	mtrx_b = mmm_init(SIZE);
+	// 	mtrx_c = mmm_init(SIZE);
 
-	// end: stuff I want to clock
+	// 	//  mmm_reset(MC, SIZE);
+	// 	double time = mmm_seq(mtrx_a, mtrx_b, mtrx_c, SIZE);
+	// 	print_boilerplate('S', time, SIZE);
+	// }
+	else
+	{
+		t_num = atoi(argv[2]);
+		SIZE = atoi(argv[3]);
+		if (t_num >= SIZE)
+		{
+			t_num = SIZE - 1;
+		}
+		printf("%d\n", SIZE);
+		mtrx_a = mmm_init(SIZE);
+		printf("**************\n");
+		mtrx_b = mmm_init(SIZE);
+		printf("**************\n");
+		mtrx_c = create_matrix();
+		printf("**************\n");
 
-	clockend = rtclock(); // stop clocking
-	printf("Time taken: %.6f sec\n", (clockend - clockstart));
+		thread_args args;
 
-	return 0;
+		pthread_t threads[t_num];
+		int portion = SIZE / t_num;
+		args.j = portion;
+
+		for (int i = 0; i < t_num; i++)
+		{
+			pthread_create(&threads[i], NULL, mmm_par, &args);
+			args.i = args.j;
+			args.j = portion * (i + 2);
+		}
+
+		for (int i = 0; i < t_num; i++)
+		{
+			pthread_join(threads[i], NULL);
+		}
+
+		printf("Resulting matrix: \n");
+		for (int i = 0; i < SIZE; i++)
+		{
+			for (int j = 0; j < SIZE; j++)
+			{
+				printf("%lf \t", mtrx_c[i][j]);
+			}
+			printf("\n");
+		}
+	}
 }
--- mmm.h
+++ mmm.h
@@ -1,11 +1,27 @@
 #ifndef MMM_H_
 #define MMM_H_
 
-void mmm_init();
-void mmm_reset(double **matrix);
+typedef struct thread_args
+{
+    int i;
+    int j;
+} thread_args;
+
+extern double **mtrx_a;
+extern double **mtrx_b;
+extern double **mtrx_c;
+extern int SIZE;
+extern int t_num;
+
+void test();
+
+double **mmm_init(int size);
+double **mmm_reset(int size);
+double **create_matrix();
 void mmm_freeup();
-void mmm_seq();
+double mmm_seq(double **MA, double **MB, double **MC, int size);
 void *mmm_par(void *args);
 double mmm_verify();
+void print_boilerplate(char mode, double time, int size);
 
 #endif /* MMM_H_ */
